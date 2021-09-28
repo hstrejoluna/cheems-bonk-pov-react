fda60104a9bde2c7ecb772e319026c6582cdfe16
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-28 11:07:06 -0500

    first commit with header :b

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from './Components/Header';
+function App() {
+  return (
+    <div className="App">
+    <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const Header = () => {
+  return (
+    <header className="header-cheems">
+        <button>About</button>
+
+      <h1>Cheems Call</h1>
+      <button>BONK!</button>
+    </header>
+  );
+};
+
+export default Header;
