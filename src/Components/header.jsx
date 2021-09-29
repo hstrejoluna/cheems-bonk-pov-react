94c5538f49980cf1423f641cffab3e9b2d013c46
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-28 22:10:14 -0500

    playing button

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 
 const useAudio = (url) => {
   const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(true);
+  const [playing, setPlaying] = useState(false);
 
   const toggle = () => setPlaying(!playing);
 
